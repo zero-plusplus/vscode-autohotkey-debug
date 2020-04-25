7745ded46d8de4971f311be45908eb4e1dcba61b
zero-plusplus <git@zero-plusplus.com>
2020-04-25 13:11:15 +0900

    Add vsix build process and the task of automatic installation

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,12 @@
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext, commands, window } from 'vscode';
 
 export const activate = function(context: ExtensionContext): void {
-  console.log('test');
+  console.log('Congratulations, your extension "helloworld-sample" is now active!');
+
+  const disposable = commands.registerCommand('vscode-ahk-debug.helloworld', () => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    window.showInformationMessage('Hello World!');
+  });
+
+  context.subscriptions.push(disposable);
 };
