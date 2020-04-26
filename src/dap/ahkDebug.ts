5c97fec55930c111feaaf55794a002c210a3697b
zero-plusplus <git@zero-plusplus.com>
2020-04-26 19:47:50 +0900

    Add method "launchRequest"

--- src/dap/ahkDebug.ts
+++ src/dap/ahkDebug.ts
@@ -1,6 +1,11 @@
 import { LoggingDebugSession } from 'vscode-debugadapter';
 import { DebugProtocol } from 'vscode-debugprotocol';
 
+interface LaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
+  program: string;
+  runtime: string;
+  stopOnEntry?: boolean;
+}
 export class AhkDebugSession extends LoggingDebugSession {
   // private static readonly THREAD_ID = 1;
   constructor() {
@@ -14,7 +19,7 @@ export class AhkDebugSession extends LoggingDebugSession {
 
     this.sendResponse(response);
   }
-  protected launchRequest(response: DebugProtocol.LaunchResponse, args: DebugProtocol.LaunchRequestArguments): void {
+  protected launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments): void {
     this.sendResponse(response);
   }
 }
