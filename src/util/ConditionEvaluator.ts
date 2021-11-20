9f736664d7347c77bb249ccf9f0def872fb0b46e
zero-plusplus <git@zero-plusplus.com>
2021-11-20 15:27:32 +0900

    Support `contains` object with right operand

--- src/util/ConditionEvaluator.ts
+++ src/util/ConditionEvaluator.ts
@@ -257,15 +257,28 @@ export class ConditionalEvaluator {
           }
         }
         else if (operatorType.type === 'ContainsOperator' && valueA instanceof dbgp.ObjectProperty) {
-          if (valueB instanceof dbgp.PrimitiveProperty || isPrimitive(valueB)) {
-            const searchValue = valueB instanceof dbgp.PrimitiveProperty ? valueB.value : valueB;
-            const isRegExp = String(searchValue).startsWith('/');
-            result = valueA.children.some((child) => {
-              if (child instanceof dbgp.PrimitiveProperty) {
-                return isRegExp ? regexCompare(child.value, searchValue) : equals(child.value, searchValue);
-              }
-              return false;
-            });
+          if (valueB instanceof dbgp.Property || isPrimitive(valueB)) {
+            if (valueB instanceof dbgp.ObjectProperty) {
+              result = valueA.children.some((child) => {
+                if (child instanceof dbgp.ObjectProperty) {
+                  return child.address === valueB.address;
+                }
+                return false;
+              });
+            }
+            else {
+              const searchValue = valueB instanceof dbgp.PrimitiveProperty ? valueB.value : valueB;
+              const isRegExp = String(searchValue).startsWith('/');
+              result = valueA.children.some((child) => {
+                if (child instanceof dbgp.PrimitiveProperty) {
+                  return isRegExp ? regexCompare(child.value, searchValue) : equals(child.value, searchValue);
+                }
+                if (child instanceof dbgp.ObjectProperty) {
+                  child.address;
+                }
+                return false;
+              });
+            }
           }
         }
 
