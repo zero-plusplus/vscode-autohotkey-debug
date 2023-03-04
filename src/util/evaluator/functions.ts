61070bd9b0bf2f09e87074b77a8a96264fa95755
zero-plusplus <git@zero-plusplus.com>
2023-03-04 15:49:01 +0900

    Support ASin

--- src/util/evaluator/functions.ts
+++ src/util/evaluator/functions.ts
@@ -240,6 +240,10 @@ copatibleFunctions_for_v2.set('Cos', cos);
 const tan = createMathFunction('tan', returnZero);
 copatibleFunctions_for_v1.set('Tan', tan);
 copatibleFunctions_for_v2.set('Tan', tan);
+
+const asin = createMathFunction('asin', returnZero);
+copatibleFunctions_for_v1.set('ASin', asin);
+copatibleFunctions_for_v2.set('ASin', asin);
 // #endregion Compatible functions with AutoHotkey
 
 // #region Compatibility functions with AutoHotkey
