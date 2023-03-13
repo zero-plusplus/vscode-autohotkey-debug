4f2790800bd0e5e998e1c665648abb825c4f407f
zero-plusplus <git@zero-plusplus.com>
2023-03-13 10:10:24 +0900

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,6 @@ import * as jest from 'ts-jest';
 const config: jest.JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePathIgnorePatterns: [ 'build/' ],
 };
 export default config;
