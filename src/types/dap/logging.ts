3491b4872b78622a91fd4b75bf9d52ee1cd51666
zero-plusplus <git@zero-plusplus.com>
2024-01-22 23:17:28 +0900

    Add logging types

---
+++ src/types/dap/logging.ts
@@ -0,0 +1,37 @@
+export const announceLevel = {
+  NONE: 0,
+  TRACE: 1,
+  DEBUG: 2,
+  INFO: 3,
+  NOTICE: 4,
+  WARN: 5,
+  ERROR: 6,
+  CRITICAL: 7,
+} as const;
+export type AnnounceLevelName = keyof typeof announceLevel;
+export type LogLevel = typeof announceLevel[AnnounceLevelName];
+export type LogCategory = 'stdout' | 'stderr' | 'console' | 'important' | (string & { ThisIsLiteralUnionTrick: any });
+export const outputCategories = {
+  stdout: 'stdout',             // Output in blue text
+  stderr: 'stderr',             // Output in red text
+  console: 'console',           // Output in yellow text
+  important: 'important',       // Output in blue text and show [notifications](https://code.visualstudio.com/api/ux-guidelines/notifications#notification-examples)
+} as const;
+export const outputDestination = {
+  TRACE: outputCategories.console,
+  DEBUG: outputCategories.console,
+  INFO: outputCategories.stdout,
+  NOTICE: outputCategories.stdout,
+  WARN: outputCategories.stdout,
+  ERROR: outputCategories.stderr,
+  CRITICAL: outputCategories.stderr,
+} as const;
+
+export interface LogMessage {
+  timestamp: Date;
+  message: string;
+}
+
+export interface Logger {
+  log: (message: LogMessage, level: LogLevel) => void;
+}
