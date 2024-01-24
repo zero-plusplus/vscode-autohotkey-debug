b4495b8b363c2c802e2a8eea40108cd668882c10
zero-plusplus <git@zero-plusplus.com>
2024-01-24 13:54:01 +0900

    Update logging types

--- src/types/dap/logging.ts
+++ src/types/dap/logging.ts
@@ -1,12 +1,10 @@
 export const announceLevel = {
-  NONE: 0,
-  TRACE: 1,
-  DEBUG: 2,
-  INFO: 3,
-  NOTICE: 4,
-  WARN: 5,
-  ERROR: 6,
-  CRITICAL: 7,
+  Off: 0,
+  Trace: 1,
+  Debug: 2,
+  Info: 3,
+  Warning: 4,
+  Error: 5,
 } as const;
 export type LogLevelName = keyof typeof announceLevel;
 export type LogLevel = typeof announceLevel[LogLevelName];
@@ -15,16 +13,13 @@ export const outputCategories = {
   stdout: 'stdout',             // Output in blue text
   stderr: 'stderr',             // Output in red text
   console: 'console',           // Output in yellow text
-  important: 'important',       // Output in blue text and show [notifications](https://code.visualstudio.com/api/ux-guidelines/notifications#notification-examples)
 } as const;
 export const outputDestination = {
-  TRACE: outputCategories.console,
-  DEBUG: outputCategories.console,
-  INFO: outputCategories.stdout,
-  NOTICE: outputCategories.stdout,
-  WARN: outputCategories.stdout,
-  ERROR: outputCategories.stderr,
-  CRITICAL: outputCategories.stderr,
+  Trace: outputCategories.console,
+  Debug: outputCategories.console,
+  Info: outputCategories.stdout,
+  Warn: outputCategories.stdout,
+  Error: outputCategories.stderr,
 } as const;
 
 export interface LogMessage {
