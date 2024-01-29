5c60434e79a529f48a7c16545b3fbd89d7ec2ae9
zero-plusplus <git@zero-plusplus.com>
2024-01-29 22:57:24 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
       ...preset.ts.default,
       rules: {
         ...preset.ts.default.rules,
+        'func-style': 'off',
         'no-undefined': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/restrict-template-expressions': [ 'error', { allowNumber: true } ],
       },
