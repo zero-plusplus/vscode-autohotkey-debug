40c13b07bac89a5f22e4ffa640bcdae6e6bd70f8
zero-plusplus <git@zero-plusplus.com>
2024-02-02 00:36:18 +0900

    Add deepDefaults util

--- src/tools/utils.ts
+++ src/tools/utils.ts
@@ -1,3 +1,5 @@
+import { MergeDeep } from 'type-fest';
+
 export const safeCall = <D, R, ARG, T extends (...args: ARG[]) => R>(callback: T, args?: ARG[], defaultValue?: D): R | D => {
   try {
     return callback(...(args ?? []));
@@ -30,4 +32,29 @@ export const timeoutPromise = async<T>(promise: Promise<T>, timeout_ms: number):
       }, timeout_ms);
     }),
   ]);
+};
+
+export const deepDefaults = <DEST extends object, S1 extends object, S2 extends object>(dest: DEST, ...sources: [ S1, S2?, ...any ]): MergeDeep<MergeDeep<S2, S1>, DEST> => {
+  for (const source of sources.reverse()) {
+    if (typeof source !== 'object') {
+      continue;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    for (const [ key, sourceValue ] of Object.entries(source)) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const destValue = dest[key];
+      if (typeof destValue === 'object' && (sourceValue && typeof sourceValue === 'object')) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        dest[key] = deepDefaults({ ...destValue }, sourceValue);
+        continue;
+      }
+
+      if (Object.hasOwn(dest, key)) {
+        continue;
+      }
+      dest[key] = sourceValue;
+    }
+  }
+  return dest as MergeDeep<MergeDeep<S2, S1>, DEST>;
 };
--- test/tools/utils.test.ts
+++ test/tools/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { TimeoutError, safeCall, sleep, timeoutPromise } from '../../src/tools/utils';
+import { TimeoutError, deepDefaults, safeCall, sleep, timeoutPromise } from '../../src/tools/utils';
 
 describe('utils', () => {
   test('safeCall', () => {
@@ -10,5 +10,17 @@ describe('utils', () => {
   });
   test('timeoutPromise / sleep', () => {
     expect(timeoutPromise(sleep(1000), 500)).rejects.toThrow(TimeoutError);
+  });
+  test('deepDefaults', () => {
+    const obj = { a: 'value', c: { d: 'value' } } as const;
+    const source1 = { a: 'overwrite?', c: { d: 'overwrite?', e: 'value' } } as const;
+    const source2 = { b: 'value' } as const;
+    const defaulted = deepDefaults(obj, source1, source2);
+    expect(defaulted.a).toBe('value');
+    expect(defaulted.b).toBe('value');
+    expect(defaulted.c).toEqual({ d: 'value', e: 'value' });
+
+    expect(source1).toEqual({ a: 'overwrite?', c: { d: 'overwrite?', e: 'value' } });
+    expect(source2).toEqual({ b: 'value' });
   });
 });
