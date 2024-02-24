f7f7791bbbe3f80526748b45fa0c7e24723f22fd
zero-plusplus <git@zero-plusplus.com>
2024-02-25 01:17:31 +0900

    Add parser/utils

---
+++ src/tools/autohotkey/parser/utils.ts
@@ -0,0 +1,11 @@
+import { GreenElement } from '../../../types/tools/autohotkey/parser/common.types';
+
+export const printGreenNode = (greenElement: GreenElement): string => {
+  const children = 'children' in greenElement ? greenElement.children : [ greenElement ];
+  return children.reduce((text, child) => {
+    if ('children' in child) {
+      return printGreenNode(child);
+    }
+    return text + child.text;
+  }, '');
+};
---
+++ test/tools/autohotkey/parser/utils.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test } from '@jest/globals';
+import { createGreenNodeFactory } from '../../../../src/tools/autohotkey/parser/greenNodeFactory';
+import { SyntaxKind } from '../../../../src/types/tools/autohotkey/parser/common.types';
+import { printGreenNode } from '../../../../src/tools/autohotkey/parser/utils';
+
+describe('utils', () => {
+  describe('printGreenNode', () => {
+    const factory = createGreenNodeFactory();
+
+    test('token', () => {
+      const token = factory.createToken(SyntaxKind.NumberLiteral, '123');
+      expect(printGreenNode(token)).toBe('123');
+    });
+    test('binary expression', () => {
+      const greenNode = factory.createNode(SyntaxKind.BinaryExpression, [
+        factory.createToken(SyntaxKind.NumberLiteral, '123'),
+        factory.createToken(SyntaxKind.HorizSpaceTrivia, ' '),
+        factory.createToken(SyntaxKind.PlusToken, '+'),
+        factory.createToken(SyntaxKind.HorizSpaceTrivia, ' '),
+        factory.createToken(SyntaxKind.NumberLiteral, '123'),
+      ]);
+      expect(printGreenNode(greenNode)).toBe('123 + 123');
+    });
+    test('nested node', () => {
+      const left = factory.createNode(SyntaxKind.BinaryExpression, [
+        factory.createToken(SyntaxKind.NumberLiteral, '123'),
+        factory.createToken(SyntaxKind.HorizSpaceTrivia, ' '),
+        factory.createToken(SyntaxKind.PlusToken, '+'),
+        factory.createToken(SyntaxKind.HorizSpaceTrivia, ' '),
+        factory.createToken(SyntaxKind.NumberLiteral, '123'),
+      ]);
+      const nestedNode = factory.createNode(SyntaxKind.BinaryExpression, [
+        left,
+        factory.createToken(SyntaxKind.HorizSpaceTrivia, ' '),
+        factory.createToken(SyntaxKind.PlusToken, '+'),
+        factory.createToken(SyntaxKind.HorizSpaceTrivia, ' '),
+        factory.createToken(SyntaxKind.NumberLiteral, '123'),
+      ]);
+      expect(printGreenNode(nestedNode)).toBe('123 + 123 + 123');
+    });
+  });
+});
