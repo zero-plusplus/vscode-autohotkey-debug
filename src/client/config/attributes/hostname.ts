e5fbcedabfa0b9c85da7114ad2238a0e03b9f067
zero-plusplus <git@zero-plusplus.com>
2024-04-04 03:08:36 +0900

    Update hostname validator

--- src/client/config/attributes/hostname.ts
+++ src/client/config/attributes/hostname.ts
@@ -1,5 +1,24 @@
-import { createStringValueValidator } from './common/string';
+import { AttributeValidator } from '../../../types/dap/config.types';
 
 export const attributeName = 'hostname';
-export const defaultValue = 'localhost';
-export const validator = createStringValueValidator(attributeName, defaultValue);
+export const defaultValue = '127.0.0.1';
+export const validator: AttributeValidator = async(createChecker) => {
+  const checker = createChecker(attributeName);
+
+  const rawAttribute = checker.get();
+  if (rawAttribute === undefined) {
+    checker.markValidated(defaultValue);
+    return Promise.resolve();
+  }
+  if (typeof rawAttribute === 'string') {
+    if (rawAttribute === 'localhost') {
+      checker.markValidated(defaultValue);
+      return Promise.resolve();
+    }
+    checker.markValidated(rawAttribute);
+    return Promise.resolve();
+  }
+
+  checker.throwTypeError('string');
+  return Promise.resolve();
+};
