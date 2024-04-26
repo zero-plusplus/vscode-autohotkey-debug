27733a017e2770f34c3dec253ba37ee6a59784aa
zero-plusplus <git@zero-plusplus.com>
2024-04-26 22:34:47 +0900

    Add compareAutoHotkeyVersionByOperator and tests

--- src/tools/autohotkey/version/compareAutoHotkeyVersion.ts
+++ src/tools/autohotkey/version/compareAutoHotkeyVersion.ts
@@ -1,37 +1,56 @@
+import { LiteralUnion } from 'type-fest';
 import { ParsedAutoHotkeyVersion, preIdList } from '../../../types/tools/autohotkey/version/common.types';
+import { parseAutoHotkeyVersion } from './parseAutoHotkeyVersion';
 
-export const compareAutoHotkeyVersion = (a: ParsedAutoHotkeyVersion, b: ParsedAutoHotkeyVersion): number => {
-  const mejorDiff = a.mejor - b.mejor;
+export const compareAutoHotkeyVersion = (a: string | ParsedAutoHotkeyVersion, b: string | ParsedAutoHotkeyVersion): number => {
+  const version_a = typeof a === 'string' ? parseAutoHotkeyVersion(a) : a;
+  const version_b = typeof b === 'string' ? parseAutoHotkeyVersion(b) : b;
+
+  const mejorDiff = version_a.mejor - version_b.mejor;
   if (mejorDiff !== 0) {
     return mejorDiff;
   }
 
-  const miniorDiff = a.minor - b.minor;
+  const miniorDiff = version_a.minor - version_b.minor;
   if (miniorDiff !== 0) {
     return miniorDiff;
   }
 
-  const patchDiff = a.patch - b.patch;
+  const patchDiff = version_a.patch - version_b.patch;
   if (patchDiff !== 0) {
     return patchDiff;
   }
 
-  if ('preId' in a && 'preId' in b) {
+  if ('preId' in version_a && 'preId' in version_b) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const preIdNumber_a = preIdList.indexOf(a.preId as any);
+    const preIdNumber_a = preIdList.indexOf(version_a.preId as any);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const preIdNumber_b = preIdList.indexOf(b.preId as any);
+    const preIdNumber_b = preIdList.indexOf(version_b.preId as any);
 
     const preIdDiff = preIdNumber_a - preIdNumber_b;
     if (preIdDiff !== 0) {
       return preIdDiff;
     }
   }
-  if ('preId' in a) {
+  if ('preId' in version_a) {
     return -1;
   }
-  if ('preId' in b) {
+  if ('preId' in version_b) {
     return 1;
   }
   return 0;
+};
+export const compareAutoHotkeyVersionByOperator = (a: string | ParsedAutoHotkeyVersion, operator: LiteralUnion<'=' | '!=' | '<' | '<=' | '>' | '>=', string>, b: string | ParsedAutoHotkeyVersion): boolean => {
+  const compareResult = compareAutoHotkeyVersion(a, b);
+
+  switch (operator) {
+    case '=': return compareResult === 0;
+    case '!=': return compareResult !== 0;
+    case '<': return compareResult < 0;
+    case '<=': return compareResult <= 0;
+    case '>': return 0 < compareResult;
+    case '>=': return 0 <= compareResult;
+    default: break;
+  }
+  return false;
 };
--- test/tools/autohotkey/version/compareAutoHotkeyVersion.test.ts
+++ test/tools/autohotkey/version/compareAutoHotkeyVersion.test.ts
@@ -1,18 +1,35 @@
 import { describe, expect, test } from '@jest/globals';
-import { compareAutoHotkeyVersion, parseAutoHotkeyVersion } from '../../../../src/tools/autohotkey';
+import { compareAutoHotkeyVersion, compareAutoHotkeyVersionByOperator, parseAutoHotkeyVersion } from '../../../../src/tools/autohotkey';
 
 describe('compareAutoHotkeyVersion', () => {
-  test.each`
-    a                 | b                 | expected
-    ${'1.0.0'}        | ${'1.0.1'}        | ${-1}
-    ${'1.0.1'}        | ${'1.0.0'}        | ${1}
-    ${'1.0.0'}        | ${'1.0.0'}        | ${0}
-    ${'2.0'}          | ${'2.0-alpha.0'}  | ${1}
-    ${'2.0'}          | ${'2.0-beta.0'}   | ${1}
-    ${'2.0'}          | ${'2.0-rc.0'}     | ${1}
-    ${'2.0-alpha.0'}  | ${'2.0-beta.0'}   | ${-1}
-    ${'2.0-alpha.0'}  | ${'2.0-rc.0'}     | ${-2}
-    `('', ({ a, b, expected }) => {
-    expect(compareAutoHotkeyVersion(parseAutoHotkeyVersion(String(a)), parseAutoHotkeyVersion(String(b)))).toBe(expected);
+  test('compareAutoHotkeyVersion', () => {
+    expect(compareAutoHotkeyVersion('1.0.0', parseAutoHotkeyVersion('1.0.1'))).toBe(-1);
+    expect(compareAutoHotkeyVersion('1.0.1', parseAutoHotkeyVersion('1.0.0'))).toBe(1);
+    expect(compareAutoHotkeyVersion('1.0.0', parseAutoHotkeyVersion('1.0.0'))).toBe(0);
+    expect(compareAutoHotkeyVersion('2.0', parseAutoHotkeyVersion('2.0-alpha.0'))).toBe(1);
+    expect(compareAutoHotkeyVersion('2.0', parseAutoHotkeyVersion('2.0-beta.0'))).toBe(1);
+    expect(compareAutoHotkeyVersion('2.0', parseAutoHotkeyVersion('2.0-rc.0'))).toBe(1);
+    expect(compareAutoHotkeyVersion('2.0-alpha.0', parseAutoHotkeyVersion('2.0-beta.0'))).toBe(-1);
+    expect(compareAutoHotkeyVersion('2.0-alpha.0', parseAutoHotkeyVersion('2.0-rc.0'))).toBe(-2);
+  });
+
+  test('compareAutoHotkeyVersionByOperator', () => {
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '=', '1.0.0')).toBe(true);
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '<', '1.0.0')).toBe(false);
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '<=', '1.0.0')).toBe(true);
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '>', '1.0.0')).toBe(false);
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '>=', '1.0.0')).toBe(true);
+
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '=', '2.0.0')).toBe(false);
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '<', '2.0.0')).toBe(true);
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '<=', '2.0.0')).toBe(true);
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '>', '2.0.0')).toBe(false);
+    expect(compareAutoHotkeyVersionByOperator('1.0.0', '>=', '2.0.0')).toBe(false);
+
+    expect(compareAutoHotkeyVersionByOperator('2.0.0', '=', '1.0.0')).toBe(false);
+    expect(compareAutoHotkeyVersionByOperator('2.0.0', '<', '1.0.0')).toBe(false);
+    expect(compareAutoHotkeyVersionByOperator('2.0.0', '<=', '1.0.0')).toBe(false);
+    expect(compareAutoHotkeyVersionByOperator('2.0.0', '>', '1.0.0')).toBe(true);
+    expect(compareAutoHotkeyVersionByOperator('2.0.0', '>=', '1.0.0')).toBe(true);
   });
 });
